d6f598e27549c856ba6eb9fcf7862dbb492fbea1
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2023-03-07 15:57:29 -0300

    Aula_92_sessão_finalizada

--- app/Http/Controllers/ClienteControlador.php
+++ app/Http/Controllers/ClienteControlador.php
@@ -28,11 +28,37 @@ class ClienteControlador extends Controller
      */
     public function store(Request $request)
     {
+        $regras =[
+            'nome'=> 'required|min:3|max:20|unique:clientes|unique:clientes', // no minimo 3 caracteres e no máximo 20 caracteres. O unique é usado para que não se psosa repetir a informação, unique:nome_tabela
+            'idade'=>'required',
+            'endereco'=>'required|min:5',
+            'email'=>'required|email',
+
+        ];
+        $mensagens =[
+            'required' =>'O atributo :attribute não pode ser vazio', // Mensagem genérica
+            'nome.required' =>'O campo Nome está vazio, favor preencher!',
+            'nome.min' =>'É necessário no mínimo 3 caracteres no campo nome.',
+            'email.required'=>'Digite um endereço de e-mail',
+            'email.email'=>'Digite um endereço de e-mail válido',
+        ];
+        $request ->validate($regras,$mensagens);
+
+
+
+
+
+        /*
         // Função usada para validar se o campo está preenchido, caso o campo está fazio o código para de ser executado, assim não
         // é exibido mensagem de erro para o usuário.
         $request ->validate([
-            'nome'=> 'required|'
+            'nome'=> 'required|min:3|max:20|unique:clientes|unique:clientes', // no minimo 3 caracteres e no máximo 20 caracteres. O unique é usado para que não se psosa repetir a informação, unique:nome_tabela
+            'idade'=>'required',
+            'endereco'=>'required|min:5',
+            'email'=>'required|email',
         ]);
+        */
+
 
         $cliente = new Cliente();
         $cliente->nome = $request->input('nome');
--- resources/views/novocliente.blade.php
+++ resources/views/novocliente.blade.php
@@ -22,42 +22,55 @@
                             @csrf
                             <div class="form-group">
                                 <label for="nome">Nome do cliente</label>
-                                    <input type="text" id="nome" class="form-control" name ="nome" placeholder="Nome do cliente">
+                                    <input type="text" id="nome" class="form-control {{$errors->has('nome') ? 'is-invalid' : ''}}" name ="nome" placeholder="Nome do cliente">
+                                    @if ($errors->has('nome'))
+                                        <div class="invalid-feedback">
+                                            {{$errors->first('nome')}}
+                                        </div> <!-- div usada para imprimir a mensagem de erro abaixo do campo. -->
+
+
+                                    @endif
                             </div>
                             <div class="form-group">
                                 <label for="idade">Idade do cliente</label>
-                                    <input type="number" id="idade" class="form-control" name ="idade" placeholder="Idade do cliente">
+                                    <input type="number" id="idade" class="form-control {{$errors->has('idade') ? 'is-invalid' : ''}}" name ="idade" placeholder="Idade do cliente">
+                                    @if ($errors->has('idade'))
+                                        <div class="invalid-feedback">
+                                            {{$errors->first('idade')}}
+                                        </div> <!-- div usada para imprimir a mensagem de erro abaixo do campo. -->
+
+                                    @endif
                             </div>
                             <div class="form-group">
                                 <label for="endereco">Endereço do cliente</label>
-                                    <input type="text" id="endereco" class="form-control" name ="endereco" placeholder="Endereço do cliente">
+                                    <input type="text" id="endereco" class="form-control {{$errors->has('endereco') ? 'is-invalid' : ''}}" name ="endereco" placeholder="Endereço do cliente">
+                                    @if ($errors->has('endereco'))
+                                        <div class="invalid-feedback">
+                                            {{$errors->first('endereco')}}
+                                        </div> <!-- div usada para imprimir a mensagem de erro abaixo do campo. -->
+
+                                    @endif
                             </div>
                             <div class="form-group">
                                 <label for="email">E-mail do cliente</label>
-                                    <input type="text" id="email" class="form-control" name ="email" placeholder="E-mail do cliente">
+                                    <input type="text" id="email" class="form-control {{$errors->has('email') ? 'is-invalid' : ''}}" name ="email" placeholder="E-mail do cliente">
+                                    @if ($errors->has('email'))
+                                        <div class="invalid-feedback">
+                                            {{$errors->first('email')}}
+                                        </div> <!-- div usada para imprimir a mensagem de erro abaixo do campo. -->
+
+                                    @endif
                             </div>
                             <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
                             <button type="cancel" class="btn btn-primary btn-sm">Cancelar</button>
                         </form>
                     </div>
-                    @if($errors->any())
-                        <div class="card-footer">
-                            <!-- Foreach feito para percorrer todos os campos atrás de erro.
-                            Caso algum campo esteja vazio, será printado na tela uma mensagem falando que o campo é requerido -->
-                            @foreach ($errors ->all() as $error)
-                                <div class="alert alert-danger" role ="alert">
-                                    {{ $error}}
-                                </div>
-                            @endforeach
-                        </div>
-                    @endif
+
                 </div>
             </div>
         </div>
     </main>
-    @if (isset($errors)) <!-- Caso algum campo esteja vazio, será printado na tela uma mensagem falando que o campo é requerido -->
-        {{var_dump($errors)}}
-    @endif
+
     <script src="{{asset('js/app.js')}}"type="text/javascript">  </script>
 </body>
 
